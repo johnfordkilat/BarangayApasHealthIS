b7e3395f717bcddbd372eb3bab3eca6c64666225
John Ford Kilat <78631202+johnfordkilat@users.noreply.github.com>
2022-05-03 10:44:08 +0800

    FIrst Commit

---
+++ BarangayApasHealthInformationSystem/BarangayApasHealthInformationSystem/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace BarangayApasHealthInformationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.houseNumberSearch = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.brgyApasHealthISDataSet = new BarangayApasHealthInformationSystem.BrgyApasHealthISDataSet();
+            this.personalInformationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personalInformationTableAdapter = new BarangayApasHealthInformationSystem.BrgyApasHealthISDataSetTableAdapters.PersonalInformationTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.houseNumberTxt = new System.Windows.Forms.TextBox();
+            this.firstNameTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.houseNumberGridView = new System.Windows.Forms.DataGridView();
+            this.updateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.brgyApasHealthISDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personalInformationBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.houseNumberGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // houseNumberSearch
+            // 
+            this.houseNumberSearch.Location = new System.Drawing.Point(37, 54);
+            this.houseNumberSearch.Name = "houseNumberSearch";
+            this.houseNumberSearch.Size = new System.Drawing.Size(149, 20);
+            this.houseNumberSearch.TabIndex = 0;
+            this.houseNumberSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.houseNumberSearch_KeyPress);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(204, 51);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // brgyApasHealthISDataSet
+            // 
+            this.brgyApasHealthISDataSet.DataSetName = "BrgyApasHealthISDataSet";
+            this.brgyApasHealthISDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personalInformationBindingSource
+            // 
+            this.personalInformationBindingSource.DataMember = "PersonalInformation";
+            this.personalInformationBindingSource.DataSource = this.brgyApasHealthISDataSet;
+            // 
+            // personalInformationTableAdapter
+            // 
+            this.personalInformationTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "House Number";
+            // 
+            // houseNumberTxt
+            // 
+            this.houseNumberTxt.Location = new System.Drawing.Point(118, 106);
+            this.houseNumberTxt.Name = "houseNumberTxt";
+            this.houseNumberTxt.Size = new System.Drawing.Size(100, 20);
+            this.houseNumberTxt.TabIndex = 4;
+            this.houseNumberTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // firstNameTxt
+            // 
+            this.firstNameTxt.Location = new System.Drawing.Point(118, 132);
+            this.firstNameTxt.Name = "firstNameTxt";
+            this.firstNameTxt.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTxt.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First Name";
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(118, 158);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTxt.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Last Name";
+            // 
+            // houseNumberGridView
+            // 
+            this.houseNumberGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.houseNumberGridView.Location = new System.Drawing.Point(287, 106);
+            this.houseNumberGridView.Name = "houseNumberGridView";
+            this.houseNumberGridView.Size = new System.Drawing.Size(365, 157);
+            this.houseNumberGridView.TabIndex = 9;
+            this.houseNumberGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.houseNumberGridView_CellClick);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(37, 195);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(181, 43);
+            this.updateButton.TabIndex = 10;
+            this.updateButton.Text = "UPDATE";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 600);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.houseNumberGridView);
+            this.Controls.Add(this.lastNameTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.firstNameTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.houseNumberTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.houseNumberSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.brgyApasHealthISDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personalInformationBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.houseNumberGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox houseNumberSearch;
+        private System.Windows.Forms.Button SearchButton;
+        private BrgyApasHealthISDataSet brgyApasHealthISDataSet;
+        private System.Windows.Forms.BindingSource personalInformationBindingSource;
+        private BrgyApasHealthISDataSetTableAdapters.PersonalInformationTableAdapter personalInformationTableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox houseNumberTxt;
+        private System.Windows.Forms.TextBox firstNameTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastNameTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView houseNumberGridView;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ BarangayApasHealthInformationSystem/BarangayApasHealthInformationSystem/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace BarangayApasHealthInformationSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(houseNumberSearch.Text == "")
+            {
+                MessageBox.Show("Please enter HOUSE NUMBER!");
+            }
+            else
+            {
+                string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jfkta\OneDrive\Documents\BarangayApasHealthInformationSystemDatabase\BrgyApasHealthIS.accdb";
+                OleDbConnection thisConnection = new OleDbConnection(connectionString);
+
+                string sql = "SELECT * FROM PersonalInformation WHERE HouseNumber LIKE '%" + houseNumberSearch.Text + "%'";
+
+                OleDbDataAdapter thisDataAdapter = new OleDbDataAdapter(sql, thisConnection);
+
+                DataSet thisDataSet = new DataSet();
+                thisDataAdapter.Fill(thisDataSet, "PersonalInformation");
+                houseNumberGridView.DataSource = thisDataSet.Tables["PersonalInformation"];
+                
+               
+               
+
+            }
+
+
+        }
+
+        private void houseNumberSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'brgyApasHealthISDataSet.PersonalInformation' table. You can move, or remove it, as needed.
+            this.personalInformationTableAdapter.Fill(this.brgyApasHealthISDataSet.PersonalInformation);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void houseNumberGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            houseNumberGridView.CurrentRow.Selected = true;
+            houseNumberTxt.Text = houseNumberGridView.Rows[e.RowIndex].Cells["HouseNumber"].Value.ToString();
+            firstNameTxt.Text = houseNumberGridView.Rows[e.RowIndex].Cells["FirstName"].Value.ToString();
+            lastNameTxt.Text = houseNumberGridView.Rows[e.RowIndex].Cells["LastName"].Value.ToString();
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jfkta\OneDrive\Documents\BarangayApasHealthInformationSystemDatabase\BrgyApasHealthIS.accdb";
+            OleDbConnection thisConnection = new OleDbConnection(connectionString);
+            thisConnection.Open();
+
+            //string sql = "UPDATE PersonalInformation set FirstName = '" + firstNameTxt.Text + "'";
+            //OleDbDataAdapter thisDataAdapter = new OleDbDataAdapter(sql, thisConnection);
+
+            OleDbCommand command = new OleDbCommand("UPDATE PersonalInformation SET HouseNumber = '" + houseNumberTxt.Text + "',FirstName = '" + firstNameTxt.Text + "',LastName = '" + lastNameTxt.Text + "' ", thisConnection);
+            command.ExecuteNonQuery();
+            thisConnection.Close();
+            MessageBox.Show("Updated Successfully");
+
+            
+   
+            
+        }
+    }
+}
